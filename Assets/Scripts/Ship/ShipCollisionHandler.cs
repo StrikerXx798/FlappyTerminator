1665784b837ee61406c9a434b123593176dace35
StrikerXx798 <str1k4@yandex.ru>
2025-08-30 22:50:49 +0300

    Remove logs

--- Assets/Scripts/Ship/ShipCollisionHandler.cs
+++ Assets/Scripts/Ship/ShipCollisionHandler.cs
@@ -12,7 +12,6 @@ public class ShipCollisionHandler : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log(other.name);
         if (other.TryGetComponent(out IInteractable interactable))
         {
             if (other.TryGetComponent(out EnemyBullet bullet))
